50e302d884e9fa797b57c4f5efaeecb499e5c43f
gaganVirk <deep.virk@outlook.co.nz>
2020-06-17 18:51:11 +1200

    Created different pages for the website

---
+++ GaganVirk/Models/Certifications.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GaganVirk.Models
+{
+    public class Certifications
+    {
+        public string ImageSrc { get; set; }
+        public string Title { get; set; }
+    }
+}
